52a7eaf689a06cf6855c00d18b274d4cbe31f4d2
HappyRashair <przemyslaw.wiczolek@happyteam.io>
2022-08-08 11:41:49 +0200

    Add new items to remove / add / exclude

--- Data/Model/InputConfig.cs
+++ Data/Model/InputConfig.cs
@@ -8,7 +8,7 @@ namespace ConfigParser.Data.Model
 {
     internal class InputConfig
     {
-        public Dictionary<string, string> AllEntries => Default.Concat(NorthEurope).ToDictionary(p => p.Key, p => p.Value);
+        public Dictionary<string, string> AllEntries => Default.Concat(WestEurope).ToDictionary(p => p.Key, p => p.Value);
 
         public Dictionary<string, string> Default { get; set; }
         public Dictionary<string, string> NorthEurope { get; set; }
